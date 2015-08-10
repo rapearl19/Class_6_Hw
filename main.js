481abb37fcb20005f25b7a5b8cf06786466d0e58
rapearl19 <alexanderpearl@natura.net>
2015-08-10 14:44:45 -0400

    second commit

---
+++ main.js
@@ -0,0 +1,55 @@
+// javascript homework
+
+//Alert string
+alert("this is my alert funcion");
+
+//console log test strings
+console.log("test");
+console.log("hw6");
+
+//Defining variables
+var name1="Alex";
+var name2="Jana";
+var num1=1;
+var num2=5;
+var num3=num1+num2;
+var bool=true;
+var array1=[1, "time", 2, "space"]
+var objects={first: "alex", last:"Pearl"}
+var sex=true
+
+//console log test num variables
+console.log(num3);
+
+//console log test array
+console.log(array1[1]);
+
+//console log test object
+console.log(objects["first"]);
+
+//addition function
+function addition(x, y, z){
+	return x*y*z
+}
+
+//Defining function as a variable
+var resfunc1=addition(num1, num2, num3)
+
+//exporting addition result
+console.log(resfunc1);
+
+//sex confirmation
+function isAlexMale(x){
+	if (sex=true) {
+		return "yes"
+	} else{
+		return "no"
+	};
+}
+//exporting isAlexMale result
+var resfunct2=isAlexMale(sex);
+console.log(resfunct2);
+
+
+
+
